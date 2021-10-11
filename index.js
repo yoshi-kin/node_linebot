579be972d96c85cae5cb9858474f6b7c7554ca07
Your Name <fujiwarayoshiki@yoshiki-no-iMac.local>
2021-10-11 21:40:14 +0900

    update_07

--- index.js
+++ index.js
@@ -6,9 +6,7 @@ const PORT = process.env.PORT || 3000
 const TOKEN = process.env.MSG_SPI_TUTORIAL_ACCESS_TOKEN
 
 app.use(express.json())
-app.use(express.urlencoded({
-    extended: true
-}))
+app.use(express.urlencoded({ extended: true }))
 
 app.get("/", (req, res) => {
     res.sendStatus(200)
@@ -20,9 +18,8 @@ app.listen(PORT, () => {
 
 app.post("/webhook", function(req, res) {
     res.send("HTTP POST request sent to the webhook URL!")
-    // If the user sends a message to your bot, send a reply message
     if (req.body.events[0].message.text === "Hello") {
-        // Message data, must be stringified
+
         const dataString = JSON.stringify({
             replyToken: req.body.events[0].replyToken,
             messages: [
@@ -37,14 +34,11 @@ app.post("/webhook", function(req, res) {
             ]
         })
     
-    
-        // Request header
         const headers = {
             "Content-Type": "application/json",
             "Authorization": "Bearer " + TOKEN
         }
     
-        // Options to pass into the request
         const webhookOptions = {
             "hostname": "api.line.me",
             "path": "/v2/bot/message/reply",
