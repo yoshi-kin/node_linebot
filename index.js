304a7fc1bad3ab1db583d2a3ec3b0804bb4b77d4
Your Name <fujiwarayoshiki@yoshiki-no-iMac.local>
2021-10-12 20:22:30 +0900

    update_11

--- index.js
+++ index.js
@@ -259,6 +259,78 @@ app.post("/webhook", function(req, res) {
                             }
                         }
                     ]
+                }
+            ]
+        })
+    
+        // Request header
+        const headers = {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + TOKEN
+        }
+    
+        // Options to pass into the request
+        const webhookOptions = {
+            "hostname": "api.line.me",
+            "path": "/v2/bot/message/reply",
+            "method": "POST",
+            "headers": headers,
+            "body": dataString
+        }
+    
+        // Define request
+        const request = https.request(webhookOptions, (res) => {
+            res.on("data", (d) => {
+                process.stdout.write(d)
+            })
+        })
+    
+        // Handle error
+        request.on("error", (err) => {
+            console.error(err)
+        })
+    
+        // Send data
+        request.write(dataString)
+        request.end()
+    }else if (req.body.events[0].message.text === "Button"){
+        const dataString = JSON.stringify({
+            replyToken: req.body.events[0].replyToken,
+            messages: [
+                {
+                    "type": "template",
+                    "altText": "This is a buttons template",
+                    "template": {
+                        "type": "buttons",
+                        "thumbnailImageUrl": "https://example.com/bot/images/image.jpg",
+                        "imageAspectRatio": "rectangle",
+                        "imageSize": "cover",
+                        "imageBackgroundColor": "#FFFFFF",
+                        "title": "Menu",
+                        "text": "Please select",
+                        "defaultAction": {
+                            "type": "uri",
+                            "label": "View detail",
+                            "uri": "http://example.com/page/123"
+                        },
+                        "actions": [
+                            {
+                              "type": "postback",
+                              "label": "Buy",
+                              "data": "action=buy&itemid=123"
+                            },
+                            {
+                              "type": "postback",
+                              "label": "Add to cart",
+                              "data": "action=add&itemid=123"
+                            },
+                            {
+                              "type": "uri",
+                              "label": "View detail",
+                              "uri": "http://example.com/page/123"
+                            }
+                        ]
+                    }
                   }
             ]
         })
