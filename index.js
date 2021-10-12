e9341605fa5688fe4febb10e244072011a602056
Your Name <fujiwarayoshiki@yoshiki-no-iMac.local>
2021-10-12 20:15:24 +0900

    update_09

--- index.js
+++ index.js
@@ -167,7 +167,7 @@ app.post("/webhook", function(req, res) {
         // Send data
         request.write(dataString)
         request.end()
-    }else if (req.body.events[0].message.text === "Carousel"){
+    }else if (req.body.events[0].message.text === "Image"){
         const dataString = JSON.stringify({
             replyToken: req.body.events[0].replyToken,
             messages: [
