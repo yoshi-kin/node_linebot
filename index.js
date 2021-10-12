a6d3ee454af41be4e9a85c9635929d1170f1d27e
Your Name <fujiwarayoshiki@yoshiki-no-iMac.local>
2021-10-12 20:14:36 +0900

    update_08

--- index.js
+++ index.js
@@ -167,5 +167,49 @@ app.post("/webhook", function(req, res) {
         // Send data
         request.write(dataString)
         request.end()
+    }else if (req.body.events[0].message.text === "Carousel"){
+        const dataString = JSON.stringify({
+            replyToken: req.body.events[0].replyToken,
+            messages: [
+                {
+                    "type": "location",
+                    "title": "my location",
+                    "address": "〒160-0004 東京都新宿区四谷一丁目6番1号", 
+                    "latitude": 35.687574,
+                    "longitude": 139.72922
+                }
+            ]
+        })
+    
+        // Request header
+        const headers = {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + TOKEN
+        }
+    
+        // Options to pass into the request
+        const webhookOptions = {
+            "hostname": "api.line.me",
+            "path": "/v2/bot/message/reply",
+            "method": "POST",
+            "headers": headers,
+            "body": dataString
+        }
+    
+        // Define request
+        const request = https.request(webhookOptions, (res) => {
+            res.on("data", (d) => {
+                process.stdout.write(d)
+            })
+        })
+    
+        // Handle error
+        request.on("error", (err) => {
+            console.error(err)
+        })
+    
+        // Send data
+        request.write(dataString)
+        request.end()
     }
 })
