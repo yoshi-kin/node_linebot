2bc65849c017fe5b4e7bef2bf590f2b5d692285c
Your Name <fujiwarayoshiki@yoshiki-no-iMac.local>
2021-10-12 20:09:26 +0900

    update_07

--- index.js
+++ index.js
@@ -62,14 +62,77 @@ app.post("/webhook", function(req, res) {
         // Send data
         request.write(dataString)
         request.end()
-    }else if (req.body.events[0].message.text === "Good") {
+    }else if (req.body.events[0].message.text === "Carousel") {
         // Message data, must be stringified
         const dataString = JSON.stringify({
             replyToken: req.body.events[0].replyToken,
             messages: [
                 {
-                    "type": "text",
-                    "text": "Have a nice day"
+                    "type": "template",
+                    "altText": "this is a carousel template",
+                    "template": {
+                        "type": "carousel",
+                        "columns": [
+                            {
+                              "thumbnailImageUrl": "https://example.com/bot/images/item1.jpg",
+                              "imageBackgroundColor": "#FFFFFF",
+                              "title": "this is menu",
+                              "text": "description",
+                              "defaultAction": {
+                                  "type": "uri",
+                                  "label": "View detail",
+                                  "uri": "http://example.com/page/123"
+                              },
+                              "actions": [
+                                  {
+                                      "type": "postback",
+                                      "label": "Buy",
+                                      "data": "action=buy&itemid=111"
+                                  },
+                                  {
+                                      "type": "postback",
+                                      "label": "Add to cart",
+                                      "data": "action=add&itemid=111"
+                                  },
+                                  {
+                                      "type": "uri",
+                                      "label": "View detail",
+                                      "uri": "http://example.com/page/111"
+                                  }
+                              ]
+                            },
+                            {
+                              "thumbnailImageUrl": "https://example.com/bot/images/item2.jpg",
+                              "imageBackgroundColor": "#000000",
+                              "title": "this is menu",
+                              "text": "description",
+                              "defaultAction": {
+                                  "type": "uri",
+                                  "label": "View detail",
+                                  "uri": "http://example.com/page/222"
+                              },
+                              "actions": [
+                                  {
+                                      "type": "postback",
+                                      "label": "Buy",
+                                      "data": "action=buy&itemid=222"
+                                  },
+                                  {
+                                      "type": "postback",
+                                      "label": "Add to cart",
+                                      "data": "action=add&itemid=222"
+                                  },
+                                  {
+                                      "type": "uri",
+                                      "label": "View detail",
+                                      "uri": "http://example.com/page/222"
+                                  }
+                              ]
+                            }
+                        ],
+                        "imageAspectRatio": "rectangle",
+                        "imageSize": "cover"
+                    }
                 }
             ]
         })
